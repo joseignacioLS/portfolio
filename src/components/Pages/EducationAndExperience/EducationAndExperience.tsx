ccde562b5eace2278643c34311258ba063cf0c97
Jose <ls.joseignacio@gmail.com>
2024-08-03 15:49:43 +0200

    change scrolls

--- src/components/Pages/EducationAndExperience/EducationAndExperience.tsx
+++ src/components/Pages/EducationAndExperience/EducationAndExperience.tsx
@@ -5,7 +5,7 @@ import {
   technologies,
 } from "@/constants/profileData";
 import ExpandableHeight from "@/components/ExpandableHeight/ExpandableHeight";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 enum EModes {
   FULL = "full",
@@ -20,10 +20,13 @@ const categoryToIcon = {
 
 const EducationAndExperience = () => {
   const [mode, setMode] = useState(EModes.FULL);
+  const cardListRef = useRef<HTMLDivElement>(null);
 
   const handleChangeFilter = (e: React.MouseEvent<HTMLElement>) => {
     const { filter } = e.currentTarget.dataset;
     if (!filter) return;
+    if (filter === mode) return;
+    cardListRef?.current?.scrollTo({ top: 0 });
     setMode(filter as EModes);
   };
   return (
@@ -51,65 +54,67 @@ const EducationAndExperience = () => {
           Educación
         </button>
       </div>
-      {educationAndExperienceItems
-        .sort((a, b) => {
-          return a.startDate.getTime() - b.startDate.getTime() > 0 ? -1 : 1;
-        })
-        .map((item) => {
-          const startDate = formatDateForCV(item.startDate);
-          const endDate = item.endDate
-            ? formatDateForCV(item.endDate)
-            : "actualidad";
-          const formattedDate =
-            startDate !== endDate ? `${startDate} - ${endDate}` : startDate;
-          return (
-            <ExpandableHeight
-              key={`${item.title} ${formattedDate}`}
-              show={mode === EModes.FULL || mode === item.category}
-              showToggleBtn={false}
-            >
-              <article
-                className={`${styles.entry} ${
-                  item.category === "education" ? styles.education : ""
-                } ${item.category === "experience" ? styles.experience : ""}`}
+      <div className={styles.cardList} ref={cardListRef}>
+        {educationAndExperienceItems
+          .sort((a, b) => {
+            return a.startDate.getTime() - b.startDate.getTime() > 0 ? -1 : 1;
+          })
+          .map((item) => {
+            const startDate = formatDateForCV(item.startDate);
+            const endDate = item.endDate
+              ? formatDateForCV(item.endDate)
+              : "actualidad";
+            const formattedDate =
+              startDate !== endDate ? `${startDate} - ${endDate}` : startDate;
+            return (
+              <ExpandableHeight
+                key={`${item.title} ${formattedDate}`}
+                show={mode === EModes.FULL || mode === item.category}
+                showToggleBtn={false}
               >
-                <div>
-                  <p className={styles.title}>{item.title}</p>
-                  <p className={styles.subtitle}>
-                    <b>{item.position}</b> @ {item.entity}
-                  </p>
-                  <p className={styles.dates}>{formattedDate}</p>
-                </div>
-                <div className={styles.stack}>
-                  {item.stack.map((stackItem) => {
-                    const { name, icon } = technologies[stackItem] as {
-                      name: string;
-                      icon?: string;
-                    };
-                    return (
-                      <span key={stackItem}>
-                        {icon && <img src={icon} />}
-                        {name}
-                      </span>
-                    );
-                  })}
-                </div>
-
-                {item.description.length > 0 && (
-                  <ExpandableHeight showContentText="+Info">
-                    {item.description.map((descLine, i) => {
+                <article
+                  className={`${styles.entry} ${
+                    item.category === "education" ? styles.education : ""
+                  } ${item.category === "experience" ? styles.experience : ""}`}
+                >
+                  <div>
+                    <p className={styles.title}>{item.title}</p>
+                    <p className={styles.subtitle}>
+                      <b>{item.position}</b> @ {item.entity}
+                    </p>
+                    <p className={styles.dates}>{formattedDate}</p>
+                  </div>
+                  <div className={styles.stack}>
+                    {item.stack.map((stackItem) => {
+                      const { name, icon } = technologies[stackItem] as {
+                        name: string;
+                        icon?: string;
+                      };
                       return (
-                        <p key={i} className={styles.descriptionLine}>
-                          {descLine}
-                        </p>
+                        <span key={stackItem}>
+                          {icon && <img src={icon} />}
+                          {name}
+                        </span>
                       );
                     })}
-                  </ExpandableHeight>
-                )}
-              </article>
-            </ExpandableHeight>
-          );
-        })}
+                  </div>
+
+                  {item.description.length > 0 && (
+                    <ExpandableHeight showContentText="+Info">
+                      {item.description.map((descLine, i) => {
+                        return (
+                          <p key={i} className={styles.descriptionLine}>
+                            {descLine}
+                          </p>
+                        );
+                      })}
+                    </ExpandableHeight>
+                  )}
+                </article>
+              </ExpandableHeight>
+            );
+          })}
+      </div>
     </section>
   );
 };
